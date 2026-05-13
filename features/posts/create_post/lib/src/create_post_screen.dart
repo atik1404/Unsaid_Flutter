040cccf1d1c8bf10b2092bfb57c0ef19f71b2c4d
Atik Faysal <atikfaysal.jatri@gmail.com>
2026-05-13 12:24:46 +0600

    feat: refactor CreatePostScreen to StatefulWidget and implement mood selection logic; add AnonymousCard widget for post header

--- features/posts/create_post/lib/src/create_post_screen.dart
+++ features/posts/create_post/lib/src/create_post_screen.dart
@@ -1,8 +1,28 @@
-import 'package:flutter/material.dart';
+import 'package:common/common.dart';
+import 'package:create_post/src/widgets/anonymous_card.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:designsystem/designsystem.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:ui/ui.dart';
 
-class CreatePostScreen extends StatelessWidget {
+class CreatePostScreen extends StatefulWidget {
   const CreatePostScreen({super.key});
+
+  @override
+  State<CreatePostScreen> createState() => _CreatePostScreenState();
+}
+
+class _CreatePostScreenState extends State<CreatePostScreen> {
+  String selectedMood = "ALL";
+  final TextEditingController _postController = TextEditingController();
+
+  _CreatePostScreenState();
+
+  @override
+  void dispose() {
+    _postController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,11 +35,98 @@ class CreatePostScreen extends StatelessWidget {
         ),
         backgroundColor: context.scaffoldTheme.backgroundColor,
         foregroundColor: context.appColors.brand,
-        elevation: 0,
+        leading: AppIcon(
+          const Icon(CupertinoIcons.clear),
+          color: context.appColors.brand,
+        ),
+        actions: [
+          AppTextButton(
+            'Post',
+            onPressed: () {
+              // Handle post creation logic here
+            },
+          ),
+        ],
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(AppSpacing.s1.h),
+          child: _buildFooter(context),
+        ),
       ),
-      body: Center(
-        child: AppText.bodyMedium('This is the Create Post Screen'),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(AppSpacing.s16.r),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const AnonymousCard(),
+              SizedBox(height: AppSpacing.s16.h),
+              AppText.bodyLarge('How are you feeling today?', textWeight: AppTextWeight.semiBold, color: context.appColors.contentPrimary),
+              SizedBox(height: AppSpacing.s12.h),
+              _buildMoodList(),
+              SizedBox(height: AppSpacing.s8.h),
+              const AppDivider(),
+              SizedBox(height: AppSpacing.s8.h),
+              _buildInputBox(context),
+            ],
+          ),
+        ),
       ),
+    );
+  }
+
+  Widget _buildMoodList() {
+    final moods = MoodType.values.map((e) => e.name).toList();
+
+    return SizedBox(
+      height: AppSpacing.s24.h,
+      child: ListView.builder(
+        physics: const BouncingScrollPhysics(),
+        scrollDirection: Axis.horizontal,
+        itemCount: moods.length,
+        itemBuilder: (context, index) {
+          final moodType = MoodType.values[index];
+          return MoodPillItem(
+            mood: moodType.name,
+            isSelected: moodType.name.toUpperCase() == selectedMood, // Replace with your selection logic
+            onTap: () {
+              setState(() {
+                if (selectedMood == moodType.name.toUpperCase()) {
+                  return;
+                }
+                selectedMood = moodType.name.toUpperCase(); // Update selected mood
+              });
+            },
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _buildInputBox(BuildContext context) {
+    return AppInputField(
+      controller: _postController,
+      hint: "What's on your mind?",
+      minLines: 5,
+      maxLines: 15,
+      keyboardType: TextInputType.multiline,
+      textInputAction: TextInputAction.newline,
+    );
+  }
+
+  Widget _buildFooter(BuildContext context) {
+    return Row(
+      children: [
+        AppIcon(
+          const Icon(CupertinoIcons.info),
+          color: context.appColors.contentSecondary,
+        ),
+        SizedBox(width: AppSpacing.s8.w),
+        AppText.captionSmall(
+          "Your post will be visible to everyone. Be mindful of what you share.",
+          textWeight: AppTextWeight.light,
+          color: context.appColors.contentSecondary,
+        ),
+      ],
     );
   }
 }
---
+++ features/posts/create_post/lib/src/widgets/anonymous_card.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:common/common.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:designsystem/designsystem.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class AnonymousCard extends StatelessWidget {
+  const AnonymousCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return _buildPostHeader(context, context.appColors.brand);
+  }
+
+  Widget _buildPostHeader(BuildContext context, Color borderColor) {
+    return Row(
+      children: [
+        AppImage.network(
+          'https://thumbs.dreamstime.com/b/futuristic-alien-portrait-sci-fi-environment-high-detail-grey-skinned-humanoid-figure-elongated-smooth-head-large-379960286.jpg?w=576',
+          width: IconSizes.prominent,
+          height: IconSizes.prominent,
+          shape: ImageShape.circle,
+          fit: BoxFit.cover,
+          borderColor: borderColor,
+          borderWidth: 1,
+          padding: EdgeInsets.all(AppSpacing.s2.r),
+        ),
+        SizedBox(width: AppSpacing.s8.w),
+        Expanded(
+          child: _buildHeaderTitle(context),
+        ),
+        SizedBox(width: AppSpacing.s8.w),
+        _buildTag(context, ""),
+      ],
+    );
+  }
+
+  Widget _buildHeaderTitle(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        AppText.bodySmall(
+          "Ghoost_8821",
+          textWeight: AppTextWeight.regular,
+          color: context.appColors.contentPrimary,
+        ),
+        AppText.captionSmall(
+          "Post anything you want to share with the world!",
+          textWeight: AppTextWeight.light,
+          color: context.appColors.contentSecondary,
+        ),
+      ],
+    );
+  }
+
+  Widget _buildTag(BuildContext context, String tag) {
+    final colors = _getTagColor(tag, context);
+    return AppTag(
+      padding: EdgeInsets.symmetric(horizontal: AppSpacing.s8.w, vertical: AppSpacing.s2.h),
+      backgroundColor: colors.$1,
+      child: AppText.captionSmall(
+        tag,
+        color: colors.$2,
+      ),
+    );
+  }
+
+  (Color, Color) _getTagColor(String tag, BuildContext context) {
+    final colors = context.modeColors;
+    final mood = MoodTypeX.fromString(tag) ?? MoodType.neutral;
+
+    return switch (mood) {
+      MoodType.love => (colors.love.backgroundColor, colors.love.textColor),
+      MoodType.angry => (colors.angry.backgroundColor, colors.angry.textColor),
+      MoodType.happy => (colors.happy.backgroundColor, colors.happy.textColor),
+      MoodType.sad => (colors.sad.backgroundColor, colors.sad.textColor),
+      MoodType.lonely => (colors.lonely.backgroundColor, colors.lonely.textColor),
+      MoodType.excited => (colors.excited.backgroundColor, colors.excited.textColor),
+      MoodType.dark => (colors.dark.backgroundColor, colors.dark.textColor),
+      MoodType.neutral => (colors.neutral.backgroundColor, colors.neutral.textColor),
+      MoodType.all => (context.appColors.backgroundPrimary, context.appColors.contentPrimary),
+      MoodType.confused => (colors.confused.backgroundColor, colors.confused.textColor),
+    };
+  }
+}
