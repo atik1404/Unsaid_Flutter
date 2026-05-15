9ce36e70bd14a417dde194d1432475a5f8ef970a
Atik Faysal <atikfaysal.jatri@gmail.com>
2026-05-15 16:04:31 +0600

    feat: Initialize storage repository on app start for improved data management

--- apps/lib/main.dart
+++ apps/lib/main.dart
@@ -3,11 +3,13 @@ import 'package:app/app_entry.dart';
 import 'package:di/di.dart';
 import 'package:get_it/get_it.dart';
 import 'package:flutter/material.dart';
+import 'package:pref_storage/pref_storage.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final getIt = GetIt.instance;
   await configureDependencies(getIt);
+  await getIt<StorageRepository>().onAppStart();
   await registerAppDiModule();
   runApp(const AppEntry());
 }
